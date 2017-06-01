e800b45ac585482e08d1bbf1f7f4e0c1b14bc234
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-06-01 15:58:35 +0200

    Stripped the parameters helper class out of the Fpmc object ; starting to converge on that side

--- HepMCWrapper/Fpmc.h
+++ HepMCWrapper/Fpmc.h
@@ -11,14 +11,14 @@
 #include "HepMC/GenParticle.h"
 #include "HepMC/GenVertex.h"
 #include "HepMC/GenPdfInfo.h"
-#include "HepMC/HerwigWrapper.h"
+//#include "HepMC/HerwigWrapper.h"
 #include "HepMC/HEPEVT_Wrapper.h"
 #include "HepMC/Version.h"
 
 #ifdef HEPMC_VERSION_CODE // HepMC v>=3
-#define HEPMC_VERSION3
 #include "HepMC/WriterAscii.h"
 #else // HepMC v2
+#define HEPMC_VERSION2
 #include "HepMC/IO_HERWIG.h"
 #endif
 
@@ -28,69 +28,23 @@
 #include "herwig.h"
 #include "fpmc.h"
 
+#include "FpmcParameters.h"
+
 #include <vector>
 #include <string>
 #include <ostream>
-#include <algorithm>
 #include <sstream>
 #include <stdexcept>
 #include <iostream>
 #include <cstring>
-#include <map>
-#include <regex>
 
 namespace fpmc
 {
   class Fpmc
   {
     public:
-      class Parameters : private std::map<std::string,std::string>
-      {
-        public:
-          Parameters() : std::map<std::string,std::string>() {}
-          Parameters( const std::map<std::string,std::string>& map ) : std::map<std::string,std::string>( map ) {}
-          ~Parameters() {}
-
-	  std::map<std::string,std::string> map() const { return *this; }
-
-          void add( const std::string& key, const std::string& value ) {
-	    auto pair = find( key );
-	    if ( pair==end() ) insert( std::make_pair( key, value ) );
-	    else pair->second = value;
-	  }
-	  void add( const std::string& key, float value ) { std::ostringstream os; os << value; add( key, os.str() ); }
-	  //void add( const std::string& key, int value ) { std::ostringstream os; os << value; add( key, os.str() ); }
-          bool has( const std::string& key ) const { return find( key )!=end(); }
-
-	  void dump( std::ostream& os=std::cout ) const {
-	    os << size() << " key(s) in the parameters list" << std::endl;
-	    for ( const auto& pair : map() ) { os << "[" << pair.first << "] \"" << pair.second << "\"" << std::endl; }
-	  }
-
-          const std::string getString( const std::string& key ) const {
-            const auto it = find( key );
-            if ( it==end() ) return "";
-            return it->second;
-          }
-          int getInt( const std::string& key ) const {
-            if ( !has( key ) ) return 0;
-            return atoi( getString( key ).c_str() );
-          }
-          long getLong( const std::string& key ) const {
-            if ( !has( key ) ) return 0;
-            return atol( getString( key ).c_str() );
-          }
-          unsigned int getFloat( const std::string& key ) const {
-            if ( !has( key ) ) return 0.;
-            return atof( getString( key ).c_str() );
-          }
-      };
-
       Fpmc( double, long int, const char* );
       ~Fpmc();
-
-      static void parseInputCard( const char*, Parameters& );
-      static void writeInputCard( const char*, const Parameters& );
 
       void begin();
       bool run();
@@ -101,10 +55,8 @@ namespace fpmc
 
     private:
       void initialiseParams();
-#ifndef HEPMC_VERSION3
+#ifdef HEPMC_VERSION2
       HepMC::IO_HERWIG conv_;
-#else
-      HepMC::HEPEVT_Wrapper conv_;
 #endif
       std::shared_ptr<HepMC::GenEvent> hepMCEvt_;
  
@@ -118,7 +70,7 @@ namespace fpmc
       unsigned int event_;
       double comEnergy_;
 
-      Parameters params_;
+      FpmcParameters params_;
 
       bool hadronize_;
       bool debug_;
---
+++ HepMCWrapper/FpmcParameters.h
@@ -0,0 +1,82 @@
+#ifndef FpmcInterface_FpmcParameters_h
+#define FpmcInterface_FpmcParameters_h
+
+#include "herwig.h"
+#include "fpmc.h"
+
+#include <string>
+#include <ostream>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <regex>
+
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+
+#include <type_traits>
+
+namespace fpmc
+{
+  /** \class Parameters
+   *
+   */
+  class FpmcParameters : private std::map<std::string,std::string>
+  {
+    public:
+      FpmcParameters();
+      FpmcParameters( const std::map<std::string,std::string>& map ) : std::map<std::string,std::string>( map ) {}
+      ~FpmcParameters() {}
+
+      static FpmcParameters parseCard( const char* filename );
+      void writeCard( const char* filename ) const;
+
+      std::map<std::string,std::string> map() const { return *this; }
+
+      void getHWPRAM( hwpram_t& ) const;
+      void getHWPROP( hwprop_t& ) const;
+      void getHWHARD( hwhard_t& ) const;
+      void getXSECT( xsect_t& ) const;
+      void getPDFS( pdfs_t& ) const;
+      void getAAANOMAL( aaanomal_t& ) const;
+      void getAAEXOTICAL( aaexotical_t& ) const;
+      void getCHIDEFPMC( chidefpmc_t& ) const;
+      void getKMR2FPMC( kmr2fpmc_t& ) const;
+      void getION( ion_t& ) const;
+      
+      void add( const std::string& key, const std::string& value ) {
+        auto pair = find( key );
+        if ( pair==end() ) insert( std::make_pair( key, value ) );
+        else pair->second = value;
+      }
+      void add( const std::string& key, float value ) { std::ostringstream os; os << value; add( key, os.str() ); }
+      //void add( const std::string& key, int value ) { std::ostringstream os; os << value; add( key, os.str() ); }
+      bool has( const std::string& key ) const { return find( key )!=end(); }
+
+      void dump( std::ostream& os=std::cout ) const {
+        os << size() << " key(s) in the parameters list" << std::endl;
+        for ( const auto& pair : map() ) { os << "[" << std::setw( 12 ) << pair.first << "]\t\"" << pair.second << "\"" << std::endl; }
+      }
+
+      const std::string getString( const std::string& key ) const {
+        const auto it = find( key );
+        if ( it==end() ) return "";
+        return it->second;
+      }
+      int getInt( const std::string& key ) const {
+        if ( !has( key ) ) return 0;
+        return std::stoi( getString( key ) );
+      }
+      long getLong( const std::string& key ) const {
+        if ( !has( key ) ) return 0;
+        return std::stol( getString( key ) );
+      }
+      double getFloat( const std::string& key ) const {
+        if ( !has( key ) ) return 0.;
+        return std::stod( getString( key ) );
+      }
+  };
+} 
+
+#endif
