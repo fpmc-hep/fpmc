029aeb86a0c1c04b51d54a876231cafd09a73714
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-06-01 15:58:35 +0200

    Stripped the parameters helper class out of the Fpmc object ; starting to converge on that side

---
+++ Fpmc/fpmc.h
@@ -0,0 +1,145 @@
+#ifndef FPMC_INC
+#define FPMC_INC
+
+//------------------------- FPMC ------------------------------
+//
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+  void hwmodini_();
+#define hwmodini hwmodini_
+  void hwchek_();
+#define hwchek hwchek_
+  void hwfxer_( int* );
+#define hwfxer hwfxer_
+
+  // PARAMETER(PI=3.141592654D0,FMCONV=0.197,XMSCUT=50.)
+  // PARAMETER(AMASS=931.D-3,PMASS=938D-3)
+  extern double pi_; // 3.141592654
+  extern double fmconv_; // 0.197
+  extern double xmscut_; // 50.
+  extern double amass_; // 931e-3
+  extern double pmass_; // 938e-3
+
+  //--- normalizations of partonic PDFs inside the pomeron:
+  typedef struct { double XPQNRM[12]; } pdfnrm_t; // warning: XPQNRM(-6:6)
+  //extern pdfnrm_t pdfnrm_;
+      
+  //--- atomic and proton numbers of the ion
+  typedef struct {
+    double AION, ZION, RBMIN;
+  } ion_t;
+  extern ion_t ion_;
+      
+  //--- flags carrying info whether the process is inclusive or exclusive:
+  typedef struct { char TYPEPR[3], TYPINT[3]; } prtype_t;
+  extern prtype_t prtype_;
+
+  //--- model parameters
+  typedef struct {
+    double GAPSPR,PROSPR,CDFFAC;
+    int    ISOFTM,NFLUX;
+  } xsect_t;
+  extern xsect_t xsect_;
+
+  //--- PDF parameter
+  typedef struct { int IFITPDF; } pdfs_t;
+  extern pdfs_t pdfs_;
+
+  //--- FLUX - POMERON + REGEON PARAMETERS
+  typedef struct {
+    double alphaP, Bpom, alphaPp, alphaR, alphaRp, Breg, Cr, zh1;
+  } prparams_t;
+  //extern prparams_t prparams_;
+
+  //--- anomalous coupling variables for anomalous AA->WW, AA-> ZZ, AA-> AA
+  typedef struct {
+    double D_KAPPA, LAMBDA, ANOMCUTOFF, A0W, ACW, A0Z, ACZ,
+           A1A, A2A;
+    int    AAANOM;
+  } aaanomal_t;
+  extern aaanomal_t aaanomal_;
+ 
+  //--- variables for exotic  AA->AA
+  typedef struct {
+    double AAM, AAQ, AAN, AAF0, AAW, AAA2;
+    int    AAEXOTIC;
+  } aaexotical_t;
+  extern aaexotical_t aaexotical_;
+      
+  //--- CHIDe
+  typedef struct {
+    double CHIDeX, CHIDeXP, CHIDeS2, CHIDeS;
+    int    CHIDeIGLU;
+  } chidefpmc_t;
+  extern chidefpmc_t chidefpmc_;
+
+  //--- KMR2
+  typedef struct {
+    double KMR2Q2CUT, KMR2SURV, KMR2SCALE;
+    int    KMR2DELTA;
+  } kmr2fpmc_t;
+  extern kmr2fpmc_t kmr2fpmc_;
+
+  //--- temporal variables
+  typedef struct { int ICOUNT; } oktemp_t;
+  //extern oktemp_t oktemp_;
+
+  //--- custom definition of subroutine hwaend()
+  void hwaend_();
+
+  void fpmc_welcome_();
+#define fpmc_welcome fpmc_welcome_
+  void fpmc_var_ini_( int* );
+#define fpmc_var_ini fpmc_var_ini_
+
+  //-------------------------------------------------------------
+  // outdated common blocks
+  //-------------------------------------------------------------
+
+  typedef struct {
+    float  RMASS, WMASS, HMASS, TMASS, MST1, MSB1, ECMS,
+           YJMIN, YJMAX, PTMIN, PTMAX, EMMIN, EMMAX,
+           DKAPPA,
+           ACW, A0W, A0Z, ACZ, A1A, A2A,
+           AAM, AAQ, AAN, AAF0, AAW, AAA2,
+           CHIDeX, CHIDeXP, CHIDeS2,
+           XI1Min, XI1Max, XI2Min, XI2Max,
+           CHIDeGapMin, CHIDeGapMax,
+           KMR2Q2CT, KMR2SRV, KMR2SCALE;
+  } myffread1_t;
+  //extern myffread1_t myffread1_;
+  typedef struct {
+    float dlambda, anom_cutoff, yww_min, yww_max,
+          q2ww_min, q2ww_max;
+  } myffread2_t;
+  //extern myffread2_t myffread2_;
+  typedef struct {
+    int OUTPUT, OUTPUTLHE, MAXEV, IPROC, NFLUX, NRN1,
+        NRN2, IFIT, ISOFTM, ZION, AION;
+    float BMIN;
+    int AAANOM, AAEXOTIC,
+        CHIDeIGL, KMR2DELTA;
+  } myffread3_t;
+  //extern myffread3_t myffread3_;
+  typedef struct { char HADR[1]; } cc0_t;
+  //extern cc0_t cc0_;
+  typedef struct { char TYPEPR[3]; } cc1_t;
+  //extern cc1_t cc1_;
+  typedef struct { char TYPINT[3]; } cc2_t;
+  //extern cc2_t cc2_;
+  typedef struct { char PART1[4]; } cc3_t;
+  //extern cc3_t cc3_;
+  typedef struct { char PART2[4]; } cc4_t;
+  //extern cc4_t cc4_;
+  typedef struct { int MODPDF1, MODPDF2; } cc5_t;
+  //extern cc5_t cc5_;
+  typedef struct { char NTNAME[32], CHIDePATH[32]; } cyfflong1_t;
+  //extern cyfflong1_t cyfflong1_;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- HepMCWrapper/Fpmc.cc
+++ HepMCWrapper/Fpmc.cc
@@ -5,10 +5,10 @@ namespace fpmc
   Fpmc::Fpmc( double comEnergy, long int seed, const char* card ) :
     herwigVerbosity_( 1 ), hepMCVerbosity_( true ), maxEventsToPrint_( 2 ),
     comEnergy_( comEnergy ),
+    params_( FpmcParameters::parseCard( card ) ),
     hadronize_( true ), debug_( false )
   {
-    initialiseParams();
-    parseInputCard( card, params_ );
+    params_.dump();
   }
 
   Fpmc::~Fpmc()
@@ -17,7 +17,7 @@ namespace fpmc
   void
   Fpmc::write( const char* out )
   {
-#ifndef HEPMC_VERSION3
+#ifdef HEPMC_VERSION2
     hepMCEvt_->write( out );
 #else // HepMC v>=3
     HepMC::WriterAscii output( out );
@@ -29,9 +29,7 @@ namespace fpmc
   void
   Fpmc::begin()
   {
-    // Use random seeds from datacard
-    // ===============================
-    // Using CLHEP engines
+    //--- use random seeds from datacard
     long seed0 = ( params_.has( "nrn1" ) ) ? params_.getLong( "nrn1" ) : -1L;
     long seed1 = ( params_.has( "nrn2" ) ) ? params_.getLong( "nrn2" ) : -1L;
     if ( seed0<0 || seed1<0 ) {
@@ -40,10 +38,10 @@ namespace fpmc
       if ( seed0<0 ) seed0 = CLHEP::RandFlat::shoot( randomEngine.get(), 1L, 10000L );
       if ( seed1<0 ) seed1 = CLHEP::RandFlat::shoot( randomEngine.get(), 1L, 10000L );
 
-      std::cout << "[FPMC Wrapper] SEEDS: " << seed0 << ", " << seed1 << std::endl;
+      if ( debug_ ) std::cout << "[FPMC Wrapper] SEEDS: " << seed0 << ", " << seed1 << std::endl;
     }
-    // ===============================
 
+    //--- print the FPMC greetings
     fpmc_welcome();
 
     if ( debug_ ) {
@@ -57,120 +55,74 @@ namespace fpmc
     // Call hwudat to set up HERWIG block data
     //hwudat();
 
-    if ( params_.has( "part1" ) ) params_.getString( "part1" ).copy( hwbmch.PART1, 8 );
-    if ( params_.has( "part2" ) ) params_.getString( "part2" ).copy( hwbmch.PART2, 8 );
+    if ( params_.has( "part1" ) ) params_.getString( "part1" ).copy( hwbmch_.PART1, 8 );
+    if ( params_.has( "part2" ) ) params_.getString( "part2" ).copy( hwbmch_.PART2, 8 );
 
-    hwproc.PBEAM1 = comEnergy_/2.;
-    hwproc.PBEAM2 = comEnergy_/2.;
+    hwproc_.PBEAM1 = comEnergy_/2.;
+    hwproc_.PBEAM2 = comEnergy_/2.;
 
-    if ( params_.has( "typepr" ) ) params_.getString( "typepr" ).copy( prtype.TYPEPR, 3 );
-    if ( params_.has( "typint" ) ) params_.getString( "typint" ).copy( prtype.TYPINT, 3 );
+    if ( params_.has( "typepr" ) ) params_.getString( "typepr" ).copy( prtype_.TYPEPR, 3 );
+    if ( params_.has( "typint" ) ) params_.getString( "typint" ).copy( prtype_.TYPINT, 3 );
 
-    if ( params_.has( "iproc" ) ) hwproc.IPROC = params_.getInt( "iproc" );
+    if ( params_.has( "iproc" ) ) hwproc_.IPROC = params_.getInt( "iproc" );
 
-    //ANSWER=UHADR 
-    //
     hadronize_ = strcmp( params_.getString( "hadr" ).c_str(), "Y" )==0;  
     std::cout << "[FPMC Wrapper] Run hadronization/showering: " << hadronize_ << std::endl; 
   
     if ( debug_ ) {
-      std::cout << "[FPMC Wrapper] PART1  = '" << hwbmch.PART1 << "'" << std::endl
-                << "[FPMC Wrapper] PART2  = '" << hwbmch.PART2 << "'" << std::endl
-                << "[FPMC Wrapper] TYPEPR = " << prtype.TYPEPR << std::endl
-                << "[FPMC Wrapper] TYPINT = " << prtype.TYPINT << std::endl
-                << "[FPMC Wrapper] IPROC  = " << hwproc.IPROC << std::endl;
+      std::cout << "[FPMC Wrapper] PART1  = '" << hwbmch_.PART1 << "'" << std::endl
+                << "[FPMC Wrapper] PART2  = '" << hwbmch_.PART2 << "'" << std::endl
+                << "[FPMC Wrapper] TYPEPR = " << prtype_.TYPEPR << std::endl
+                << "[FPMC Wrapper] TYPINT = " << prtype_.TYPINT << std::endl
+                << "[FPMC Wrapper] IPROC  = " << hwproc_.IPROC << std::endl;
     }
-    //CALL HWIGIN
-    //
-    hwigin();
+    hwigin_();
 
-    hwevnt.NRN[0] = seed0;
-    hwevnt.NRN[1] = seed1;
-    hwpram.EFFMIN = 1.e-6;
-    hwevnt.MAXER = 100000000; // O(inf)
-    hwpram.LWSUD = 0;         // don't write Sudakov form factors
-    hwdspn.LWDEC = 0;         // don't write three/four body decays
+    params_.getHWPRAM( hwpram_ );
+    hwpram_.IPRINT = herwigVerbosity_;
+    hwpram_.EFFMIN = 1.e-6;
+    hwpram_.MODPDF[1] = -111;
+    hwpram_.MODPDF[0] = -111;
+    hwpram_.LWSUD = 0; // don't write Sudakov form factors
+
+    hwevnt_.NRN[0] = seed0;
+    hwevnt_.NRN[1] = seed1;
+    hwevnt_.MAXER = 100000000; // O(inf)
+    hwdspn_.LWDEC = 0; // don't write three/four body decays
     // (no fort.77 and fort.88 ...)
 
     // Init LHAPDF glue
-    std::memset(hwprch.AUTPDF, ' ', sizeof(hwprch.AUTPDF));
-    for ( unsigned int i = 0; i < 2; i++ ) {
-      hwpram.MODPDF[i] = -111;
-      std::memcpy(hwprch.AUTPDF[i], "HWLHAPDF", 8);
+    const std::string pdfset( "HWLHAPDF" );
+    for ( unsigned int i=0; i<2; i++ ) {
+      pdfset.copy( hwprch_.AUTPDF[i], 8 );
     }
 
-    hwevnt.MAXPR = maxEventsToPrint_;
-    hwpram.IPRINT = herwigVerbosity_;
+    hwevnt_.MAXPR = maxEventsToPrint_;
 
-    if ( params_.has( "modpdf1" ) ) hwpram.MODPDF[0] = params_.getInt( "modpdf1" );
-    if ( params_.has( "modpdf2" ) ) hwpram.MODPDF[1] = params_.getInt( "modpdf2" );
+    params_.getHWPROP( hwprop_ );
+    params_.getHWHARD( hwhard_ );
 
-    if ( params_.has( "hmass" ) ) hwprop.RMASS[201] = params_.getFloat( "hmass" ); // higgs mass
-    if ( params_.has( "tmass" ) ) hwprop.RMASS[6]   = params_.getFloat( "tmass" ); // top mass
-    if ( params_.has( "wmass" ) ) hwprop.RMASS[198] = params_.getFloat( "wmass" ); // W mass
-    if ( params_.has( "mst1" ) ) hwprop.RMASS[406] = params_.getFloat( "mst1" ); // stop1 mass
-    if ( params_.has( "msb1" ) ) hwprop.RMASS[405] = params_.getFloat( "msb1" ); // stop2 mass
+    params_.getXSECT( xsect_ );
+    params_.getPDFS( pdfs_ );
+    params_.getAAANOMAL( aaanomal_ );
+    params_.getAAEXOTICAL( aaexotical_ );
 
-    if ( params_.has( "q2wwmn" ) ) hwhard.Q2WWMN = params_.getFloat( "q2wwmn" );
-    if ( params_.has( "q2wwmx" ) ) hwhard.Q2WWMX = params_.getFloat( "q2wwmx" );
-    if ( params_.has( "ywwmin" ) ) hwhard.YWWMIN = params_.getFloat( "ywwmin" );
-    if ( params_.has( "ywwmax" ) ) hwhard.YWWMAX = params_.getFloat( "ywwmax" );
-    if ( params_.has( "yjmin" ) ) hwhard.YJMIN = params_.getFloat( "yjmin" );
-    if ( params_.has( "yjmax" ) ) hwhard.YJMAX = params_.getFloat( "yjmax" );
-    if ( params_.has( "ptmin" ) ) hwhard.PTMIN = params_.getFloat( "ptmin" );
-    if ( params_.has( "ptmax" ) ) hwhard.PTMAX = params_.getFloat( "ptmax" );
-    if ( params_.has( "emmin" ) ) hwhard.EMMIN = params_.getFloat( "emmin" );
+    params_.getCHIDEFPMC( chidefpmc_ );
+    chidefpmc_.CHIDeS = comEnergy_*comEnergy_;
+    params_.getKMR2FPMC( kmr2fpmc_ );
 
-    if ( params_.has( "nflux" ) ) xsect.NFLUX = params_.getInt( "nflux" );
-    if ( params_.has( "ifit" ) ) pdfs.IFITPDF = params_.getInt( "ifit" );
+    params_.getION( ion_ );
 
-    if ( params_.has( "aaanom" ) ) aaanomal.AAANOM = params_.getInt( "aaanom" );
-    if ( params_.has( "dkappa" ) ) aaanomal.D_KAPPA = params_.getFloat( "dkappa" );
-    if ( params_.has( "dlambda" ) ) aaanomal.LAMBDA = params_.getFloat( "dlambda" );
-    if ( params_.has( "a0w" ) ) aaanomal.A0W = params_.getFloat( "a0w" );
-    if ( params_.has( "acw" ) ) aaanomal.ACW = params_.getFloat( "acw" );
-    if ( params_.has( "a0z" ) ) aaanomal.A0Z = params_.getFloat( "a0z" );
-    if ( params_.has( "acz" ) ) aaanomal.ACZ = params_.getFloat( "acz" );
-    if ( params_.has( "a1a" ) ) aaanomal.A1A = params_.getFloat( "a1a" );
-    if ( params_.has( "a2a" ) ) aaanomal.A2A = params_.getFloat( "a2a" );
-    if ( params_.has( "anomcutoff" ) ) aaanomal.ANOMCUTOFF = params_.getInt( "anomcutoff" );
-
-    if ( params_.has( "aaexotic" ) ) aaexotical.AAEXOTIC = params_.getInt( "aaexotic" );
-    if ( params_.has( "aam" ) ) aaexotical.AAM = params_.getFloat( "aam" );
-    if ( params_.has( "aaq" ) ) aaexotical.AAQ = params_.getFloat( "aaq" );
-    if ( params_.has( "aan" ) ) aaexotical.AAN = params_.getFloat( "aan" );
-    if ( params_.has( "aaf0" ) ) aaexotical.AAF0 = params_.getFloat( "aaf0" );
-    if ( params_.has( "aaw" ) ) aaexotical.AAW  = params_.getFloat( "aaw" );
-    if ( params_.has( "aaa2" ) ) aaexotical.AAA2 = params_.getFloat( "aaa2" );
-
-    if ( params_.has( "chideiglu" ) ) chidefpmc.CHIDeIGLU = params_.getInt( "chideiglu" );
-    if ( params_.has( "chidex" ) ) chidefpmc.CHIDeX = params_.getFloat( "chidex" );
-    if ( params_.has( "chidexp" ) ) chidefpmc.CHIDeXP = params_.getFloat( "chidexp" );
-    if ( params_.has( "chides2" ) ) chidefpmc.CHIDeS2 = params_.getFloat( "chides2" );
-    chidefpmc.CHIDeS = comEnergy_*comEnergy_;
-  
-    if ( params_.has( "kmr2delta" ) ) kmr2fpmc.KMR2DELTA = params_.getFloat( "kmr2delta" );
-    if ( params_.has( "kmr2q2cut" ) ) kmr2fpmc.KMR2Q2CUT = params_.getFloat( "kmr2q2cut" );
-    if ( params_.has( "kmr2surv" ) ) kmr2fpmc.KMR2SURV = params_.getFloat( "kmr2surv" );
-    if ( params_.has( "kmr2scale" ) ) kmr2fpmc.KMR2SCALE = params_.getFloat( "kmr2scale" );
-
-    if ( params_.has( "isoftm" ) ) xsect.ISOFTM = params_.getInt( "isoftm" );
-
-    if ( params_.has( "zion" ) ) ion.ZION = params_.getInt( "zion" );
-    if ( params_.has( "aion" ) ) ion.AION = params_.getInt( "aion" );
-    if ( params_.has( "ubmin" ) ) ion.RBMIN = params_.getFloat( "ubmin" );
-
-    //--- Initialize model/pdf dependant parameters
+    //--- initialize model/pdf dependant parameters
     hwmodini();
 
-    //--- Compute parameter dependent constants
+    //--- compute parameter dependent constants
     hwuinc();
 
-    //--- Check POMWIG Settings + Initialisations for consistency
-    //
+    //--- check POMWIG Settings + Initialisations for consistency
     hwchek();
 
-    //--- Call HWUSTA to make any particle stable
+    //--- call HWUSTA to make any particle stable
     int iopt = 1;
     int iwig = 0;
     char nwig[9] = "        ";
@@ -179,13 +131,15 @@ namespace fpmc
     hwuidt( &iopt, &ipdg, &iwig, nwig );
     if ( ipdg ) hwusta(nwig, 1);
  
-    //--- Initialize elementary process
+    //--- initialize elementary process
     hweini();
 
-    //---Initialize event record fixing : this will replace the beam 
-    //   electrons by protons, radiated photons by pomerons/reggeons etc
+    //--- initialize event record fixing
+    // this will replace the beam electrons by protons, radiated photons by pomerons/reggeons, etc.
     int init = 1;
     hwfxer( &init );
+
+    params_.writeCard( "lastrun.card" );
   }
 
   void
@@ -195,7 +149,10 @@ namespace fpmc
   bool
   Fpmc::run()
   {
-    // Call herwig routines to create HEPEVT
+#ifndef HEPMC_VERSION2
+    HepMC::HEPEVT_Wrapper::zero_everything();
+#endif
+    //--- call herwig routines to create HEPEVT
 
     hwuine(); // initialize event
 
@@ -218,31 +175,33 @@ namespace fpmc
     int init = 0;
     hwfxer( &init ); // fix event record (i.e. restore correct intermediate states)
 
-    if ( hwevnt.IERROR ) return false;
-#ifndef HEPMC_VERSION3
+    if ( hwevnt_.IERROR ) return false;
+#ifdef HEPMC_VERSION2
     hepMCEvt_ = std::make_shared<HepMC::GenEvent>( *conv_.read_next_event() );
 #else
-    conv_.print_hepevt();
-    conv_.HEPEVT_to_GenEvent( hepMCEvt_.get() );
+    std::cout << "---> " << HepMC::HEPEVT_Wrapper::number_entries() << std::endl;
+    //HepMC::HEPEVT_Wrapper::set_number_entries( 4000 );
+    //HepMC::HEPEVT_Wrapper::set_sizeof_real( 8 );
+    //HepMC::HEPEVT_Wrapper::print_hepevt();
+    //HepMC::HEPEVT_Wrapper::HEPEVT_to_GenEvent( hepMCEvt_.get() );
 #endif
-    ++event_;
 
-    hepMCEvt_->set_event_number(event_ - 1);
-#ifndef HEPMC_VERSION3
-    hepMCEvt_->set_signal_process_id( hwproc.IPROC );
+    hepMCEvt_->set_event_number( event_-1 );
+#ifdef HEPMC_VERSION2
+    hepMCEvt_->set_signal_process_id( hwproc_.IPROC );
     hepMCEvt_->set_event_scale( -1. );
 #endif
-    hepMCEvt_->weights().push_back( hwevnt.EVWGT );
+    hepMCEvt_->weights().push_back( hwevnt_.EVWGT );
 
-#ifndef HEPMC_VERSION3
+#ifdef HEPMC_VERSION2
     HepMC::PdfInfo pdfInfo;
-    pdfInfo.set_x1( hwhard.XX[0] );
-    pdfInfo.set_x2( hwhard.XX[1] );
-    pdfInfo.set_scalePDF( hwhard.EMSCA );
+    pdfInfo.set_x1( hwhard_.XX[0] );
+    pdfInfo.set_x2( hwhard_.XX[1] );
+    pdfInfo.set_scalePDF( hwhard_.EMSCA );
     hepMCEvt_->set_pdf_info( pdfInfo );
 #else
     auto pdf_info = std::make_shared<HepMC::GenPdfInfo>();
-    pdf_info->set( 0, 0, hwhard.XX[0], hwhard.XX[1], hwhard.EMSCA, 0, 0 );
+    pdf_info->set( 0, 0, hwhard_.XX[0], hwhard_.XX[1], hwhard_.EMSCA, 0, 0 );
     hepMCEvt_->set_pdf_info( pdf_info );
 #endif
 
@@ -256,141 +215,21 @@ namespace fpmc
     for(HepMC::GenEvent::particle_const_iterator it = event()->particles_begin(); (it != event()->particles_end() && targetParton==NULL); it++)
       if((*it)->status()==122) targetParton = (*it);*/
 
-
+#ifdef HEPMC_VERSION2
     //******** Verbosity ********
     if ( event_<=maxEventsToPrint_ && hepMCVerbosity_ ) {
-#ifndef HEPMC_VERSION3
       // Prints HepMC event
       std::ostringstream oss;
       oss << "\n----------------------" << endl	
           << "Event process id = " << hepMCEvt_->signal_process_id() << endl; 
       std::cout << oss.str();
       hepMCEvt_->print();
-#endif
     }
+#endif
+
+    //--- increment the events counter
+    ++event_;
 
     return true;
-  }
-
-  void
-  Fpmc::parseInputCard( const char* filename, Fpmc::Parameters& params )
-  {
-    std::ifstream card( filename );
-    std::string buf;
-    //std::regex rgx_parse( "(\\w+)\\s+(\\S+)" );
-    std::regex rgx_parse( "(\\w+)[ ']+([^'\\n\\t]+)" );
-    std::smatch match;
-    while ( !card.eof() ) {
-      std::getline( card, buf );
-      if ( !std::regex_match( buf, match, rgx_parse ) ) continue;
-      std::string key = match[1];
-      std::transform( key.begin(), key.end(), key.begin(), ::tolower );
-      params.add( key, match[2] );
-    }
-  }
-
-  void
-  Fpmc::writeInputCard( const char* output, const Fpmc::Parameters& params )
-  {
-    std::ofstream out( output );
-    for ( const auto& pair : params.map() ) {
-      std::ostringstream os;
-      std::string key;
-      std::transform( pair.first.begin(), pair.first.end(), key.begin(), ::toupper );
-      os << std::setw( 10 ) << key
-         << std::setw( 50 ) << pair.second << std::endl;
-      out << os.str();
-    }
-    out.close();
-  }
-
-  void
-  Fpmc::initialiseParams()
-  {
-    params_.add( "rmass", 0. );
-    params_.add( "wmass", 80.425 );
-    params_.add( "rmass", 0. );
-    params_.add( "wmass", 80.425 );
-    params_.add( "hmass", 125.0 );
-    params_.add( "tmass", 174.3 );
-    params_.add( "mst1", 250. );
-    params_.add( "msb1", 250. );
-    params_.add( "ecms", 14.e3 );
-    params_.add( "yjmin", -6. );
-    params_.add( "yjmax", 6. );
-    params_.add( "ptmin", 0. );
-    params_.add( "ptmax", 1.e8 );
-    params_.add( "emmin", 10. );
-    params_.add( "emmax", 1.e8 );
-    params_.add( "dkappa", 0. );
-    params_.add( "acw", 0. );
-    params_.add( "a0w", 0. );
-    params_.add( "a0z", 0. );
-    params_.add( "acz", 0. );
-    params_.add( "a1a", 0. );
-    params_.add( "a2a", 0. );
-    params_.add( "aam", 0. );
-    params_.add( "aaq", 0. );
-    params_.add( "aan", 0. );
-    params_.add( "aaf0", 0. );
-    params_.add( "aaf0z", 0. );
-    params_.add( "aaf0w", 0. );
-    params_.add( "aaf0zg", 0. );
-    params_.add( "aaw", 0. );
-    params_.add( "aaa2", 0. );
-    params_.add( "chidex", -1. );
-    params_.add( "chidexp", -1. );
-    params_.add( "chides2", -1. );
-    params_.add( "xi1min", -1. );
-    params_.add( "xi1max", -1. );
-    params_.add( "xi2min", -1. );
-    params_.add( "xi2max", -1. );
-    params_.add( "chidegapmin", 0. );
-    params_.add( "chidegapmax", 0. );
-    params_.add( "kmr2q2cut", 2. );
-    params_.add( "kmr2surv", 0.3 );
-    params_.add( "kmr2scale", 0.618 );
-    //
-    params_.add( "dlambda", 0. );
-    params_.add( "anomcutoff", -1 );
-    params_.add( "ywwmin", 0. );
-    params_.add( "ywwmax", 0.1 );
-    params_.add( "q2wwmn", 0. );
-    params_.add( "q2wwmx", 4. );
-    //
-    params_.add( "output", 1 );
-    params_.add( "outputlhe", 0 );
-    params_.add( "maxev", 1000 );
-    params_.add( "iproc", 16010 );
-    params_.add( "nflux", 15 );
-    params_.add( "nrn1", 33799 );
-    params_.add( "nrn2", 11799 );
-    params_.add( "ifit", 10 );
-    params_.add( "isoftm", 1 );
-    params_.add( "zion", 1 );
-    params_.add( "aion", 1 );
-    params_.add( "bmin", 1. );
-    params_.add( "aaanom", 0 );
-    params_.add( "aaexotic", 0 );
-    params_.add( "chide_iglu", -1 );
-    params_.add( "kmr2_delta", 1 );
-    //
-    params_.add( "hadr", "Y" );
-    //
-    params_.add( "typepr", "EXC" );
-    //
-    params_.add( "typint", "QED" );
-    //
-    params_.add( "part1", "E+" );
-    //
-    params_.add( "part2", "E+" );
-    //
-    params_.add( "modpdf1", -1 );
-    params_.add( "modpdf2", -1 );
-    //
-    params_.add( "ntname", "tmpntuple.ntp" );
-    params_.add( "chidepath", "External/CHIDe/Data/" );
-    //
-    params_.add( "lhefile", "FPMC.lhe" );
   }
 }
