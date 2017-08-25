12caf88f85dc33d82ca7fe35f81dc6babf81be6a
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-08-25 18:44:49 +0200

    Simplified the test

--- Examples/fpmc_bare.cc
+++ Examples/fpmc_bare.cc
@@ -6,13 +6,20 @@ using namespace std;
 
 int main( int argc, char* argv[] )
 {
-  fpmc::Fpmc generator( argv[1] );
+  //fpmc::Fpmc gen( argv[1] );
+  fpmc::Fpmc gen;
+  gen.parameters().setSqrtS( 13.e3 ); // in GeV
+  gen.parameters().setProcessId( 16008 ); // QED yy->mu,mu
+  gen.parameters().setProcessType( fpmc::ExclusiveProcess );
+  gen.parameters().setInteractionType( fpmc::QED );
+  gen.parameters().setIntermediateFlux( fpmc::PhotonPhotonBudnevCoherent );
+  gen.parameters().setPtRange( 10. ); // no upper limit
 
-  generator.parameters().dump();
+  gen.parameters().dump();
 
-  for( unsigned int evt = 0; evt < 10; ++evt ) {
+  for( unsigned int evt = 0; evt < 10e5; ++evt ) {
     cout << "[FPMC] Processing event " << ( evt+1 ) << endl;
-    generator.next();
+    gen.next();
   }
   return 0;
 }
