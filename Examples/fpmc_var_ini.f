a5f1f420bbe4b7266dcb39aeae1ccb1765e53b99
Laurent Forthomme <laurent.forthomme@cern.ch>
2018-06-25 14:46:52 +0200

    Allowing to skip CERNLIB's FFREAD dependency if no installation has been found

--- Examples/fpmc_var_ini.f
+++ Examples/fpmc_var_ini.f
@@ -4,7 +4,7 @@ C * Routine initialize FPMC parameters to default
 C * if REACARD = .TRUE. it allows to read setup from external format
 C * free card (FFC) with
 C * ./module < datacard.ffcard
-C----------------------------------------------------------------------- 
+C-----------------------------------------------------------------------
 
       SUBROUTINE FPMC_VAR_INI(READCARD)
       IMPLICIT NONE
@@ -17,79 +17,79 @@ C-----------------------------------------------------------------------
       CALL FPMC_WELCOME
 
 c---FFC default initialiszation
-      UOUTPUT    =    1
-      UOUTPUTLHE =    0
-      UNTNAME    =    'tmpntuple.ntp'
-      UMAXEV     =    1000
-      UTYPEPR    =    'EXC'
-      UTYPINT    =    'QED'
-      UHADR      =    'Y'
-      UPART1     =    'E+'
-      UPART2     =    'E+'
-      UIPROC     =    16010
-      UHMASS     =    120.
-      UNFLUX     =    15
-      UNRN1      =    33799
-      UNRN2      =    11799
-      UWMASS     =    80.425
-      UTMASS     =    174.3
-      UMST1      =    250.
-      UMSB1      =    250.
-      UECMS      =    14.E3
-      UPTMIN     =    0.
-      UPTMAX     =    1.E8
-      UYJMIN     =    -6.
-      UYJMAX     =    6.
-      UEMMIN     =    10.
-      UEMMAX     =    1.E8
-      UIFIT      =    10
-      UISOFTM    =    1
-      UZION      =    1
-      UAION      =    1
-      UBMIN      =    1.
-      UAAANOM  =    0       ! original HERWIG AAWW formula
-      UDKAPPA    =    0.
-      UDLAMBDA   =    0.
-      UA0W       =    0.
-      UACW       =    0.
-      UA0Z       =    0.
-      UACZ       =    0.
-      UA1A       =    0.
-      UA2A       =    0.
-      UANOMCUTOFF =  -1.   ! if < 0 turns formfactor off
-      UAAEXOTIC  =    0    ! SM Excl AAAA
-      UAAM       =    0.
-      UAAQ       =    0.
-      UAAN       =    0.
-      UAAF0      =    0.
-      UAAF0Z     =    0.    !C Baldenegro 05-2016 spin-0 resonance, phi->zz coupling
-      UAAF0W     =    0.    !C Baldenegro 05-2016 spin-0 resonance, phi->zz coupling
-      UAAF0ZG    =    0.    !C Baldenegro 05-2016 Spin-0 to ZGamma coupling
-      UAAW       =    0.
-      UAAA2      =    0.
-      UQ2WWMN    =    0.
-      UQ2WWMX    =    4.
-      UYWWMIN    =    0.
-      UYWWMAX    =    0.1
-      UCHIDeIGLU = -1
-      UCHIDeX    = -1.0
-      UCHIDeXP   = -1.0
-      UCHIDeS2   = -1.0
-      UXI1MIN= -1.0 
-      UXI1MAX= -1.0 
-      UXI2MIN= -1.0 
-      UXI2MAX= -1.0 
-      UKMR2Q2CUT=2.0
-      UKMR2SURV=0.03
-      UKMR2SCALE=0.618!1.0
-      UKMR2DELTA=1!2
-      UCHIDePATH='External/CHIDe/Data/'
-      UMODPDF1   = -1
-      UMODPDF2   = -1
-      ULHEFILE   = 'FPMC.lhe'
+      UOUTPUT     =    1
+      UOUTPUTLHE  =    0
+      UNTNAME     =    'tmpntuple.ntp'
+      UMAXEV      =    1000
+      UTYPEPR     =    'EXC'
+      UTYPINT     =    'QED'
+      UHADR       =    'Y'
+      UPART1      =    'E+'
+      UPART2      =    'E+'
+      UIPROC      =    16010
+      UHMASS      =    120.
+      UNFLUX      =    15
+      UNRN1       =    33799
+      UNRN2       =    11799
+      UWMASS      =    80.425
+      UTMASS      =    174.3
+      UMST1       =    250.
+      UMSB1       =    250.
+      UECMS       =    14.E3
+      UPTMIN      =    0.
+      UPTMAX      =    1.E8
+      UYJMIN      =    -6.
+      UYJMAX      =    6.
+      UEMMIN      =    10.
+      UEMMAX      =    1.E8
+      UIFIT       =    10
+      UISOFTM     =    1
+      UZION       =    1
+      UAION       =    1
+      UBMIN       =    1.
+      UAAANOM     =    0       ! original HERWIG AAWW formula
+      UDKAPPA     =    0.
+      UDLAMBDA    =    0.
+      UA0W        =    0.
+      UACW        =    0.
+      UA0Z        =    0.
+      UACZ        =    0.
+      UA1A        =    0.
+      UA2A        =    0.
+      UANOMCUTOFF =    -1.     ! if < 0 turns formfactor off
+      UAAEXOTIC   =    0       ! SM Excl AAAA
+      UAAM        =    0.
+      UAAQ        =    0.
+      UAAN        =    0.
+      UAAF0       =    0.
+      UAAF0Z      =    0.      !C Baldenegro 05-2016 spin-0 resonance, phi->zz coupling
+      UAAF0W      =    0.      !C Baldenegro 05-2016 spin-0 resonance, phi->zz coupling
+      UAAF0ZG     =    0.      !C Baldenegro 05-2016 Spin-0 to ZGamma coupling
+      UAAW        =    0.
+      UAAA2       =    0.
+      UQ2WWMN     =    0.
+      UQ2WWMX     =    4.
+      UYWWMIN     =    0.
+      UYWWMAX     =    0.1
+      UCHIDeIGLU  =    -1
+      UCHIDeX     =    -1.0
+      UCHIDeXP    =    -1.0
+      UCHIDeS2    =    -1.0
+      UXI1MIN     =    -1.0
+      UXI1MAX     =    -1.0
+      UXI2MIN     =    -1.0
+      UXI2MAX     =    -1.0
+      UKMR2Q2CUT  =    2.0
+      UKMR2SURV   =    0.03
+      UKMR2SCALE  =    0.618!1.0
+      UKMR2DELTA  =    1!2
+      UCHIDePATH  =    'External/CHIDe/Data/'
+      UMODPDF1    =    -1
+      UMODPDF2    =    -1
+      ULHEFILE    =    'FPMC.lhe'
 
       IF(READCARD) THEN
-        !---Key reading      
+        !---Key reading
         PRINT *, "Reading datacard ..."
         call FFINIT(1000)
         !---JC read config file
@@ -168,7 +168,7 @@ c---FFC default initialiszation
         call FFKEY('LHEFILE',ULHEFILE,64,'mixed')
 
         call FFGO
-       
+
 C    Xi cuts
           IF(UXI1Min.LT.0.0) UXI1Min = UYWWMIN
           IF(UXI1Max.LT.0.0) UXI1Max = UYWWMAX
@@ -176,14 +176,14 @@ C    Xi cuts
           IF(UXI2Max.LT.0.0) UXI2Max = UYWWMAX
 
 C     CHIDe Model
-        IF(UNFLUX.EQ.18) THEN          
+        IF(UNFLUX.EQ.18) THEN
 C   Impact factor parameterisation
           IF(UCHIDeIGLU.LT.0.0) UCHIDeIGLU = 4
 C   Scaling lower limit of Sudakov factor integration
           IF(UCHIDeXP.LT.0.0.AND.(.NOT.(UIPROC.EQ.16059)))UCHIDeXP=0.5
           IF(UCHIDeXP.LT.0.0 .AND. UIPROC.EQ.16059) UCHIDeXP = 1.0
 C   Gap Survival Probability
-          IF(UCHIDeS2.LT.0.0) THEN 
+          IF(UCHIDeS2.LT.0.0) THEN
             IF(abs(UECMS-14000.).lt.1.) THEN
               UCHIDeS2 = 0.03 ! ~LHC
             ELSEIF(abs(UECMS-1960.).lt.1) THEN
@@ -197,22 +197,22 @@ C   Gap Survival Probability
             ENDIF
           ENDIF
           IF(UIPROC.EQ.16012) THEN ! Scaling upper limit of Sudakov factor integration:
-            IF(UCHIDeX.LT.0.0) UCHIDeX = 0.5      
+            IF(UCHIDeX.LT.0.0) UCHIDeX = 0.5
           ELSEIF(UIPROC.EQ.16059) THEN
             IF(UCHIDeX.LT.0.0) UCHIDeX = 0.5
           ELSEIF(UIPROC.EQ.19999) THEN
-            IF(UCHIDeX.LT.0.0) UCHIDeX = 1    
+            IF(UCHIDeX.LT.0.0) UCHIDeX = 1
           ENDIF
        ENDIF
-       
-       IF(UNFLUX.EQ.16) THEN 
+
+       IF(UNFLUX.EQ.16) THEN
          IF(UCHIDeS2.GT.0) UKMR2SURV = UCHIDeS2
-         IF(UCHIDeS2.LT.0) UKMR2SURV = 0.03  
+         IF(UCHIDeS2.LT.0) UKMR2SURV = 0.03
        ENDIF
 
       ELSE
         PRINT *, "Reading datacard not requested, using default",
      & " parameters."
-      ENDIF    
+      ENDIF
 
       END SUBROUTINE
