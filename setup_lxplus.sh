e317bb828b7d962aff91105be9681164f835178c
Gustavo <diemort@gmail.com>
2022-04-12 17:24:01 +0200

    updating source file for gcc v8 link

--- setup_lxplus.sh
+++ setup_lxplus.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 # Configuration files to properly set the dependencies on LXPLUS machines with CentOS 7:
-source /cvmfs/sft.cern.ch/lcg/external/gcc/6.2.0/x86_64-centos7-gcc62-opt/setup.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_93python3/tbb/2018_U1/x86_64-centos7-gcc62-opt/tbb-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_93python3/ROOT/6.12.06/x86_64-centos7-gcc62-opt/bin/thisroot.sh 
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/8/x86_64-centos7-gcc8-opt/setup.sh
